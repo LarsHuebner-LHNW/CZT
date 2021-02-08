deac50553cdf61f22faa1a3c5c81484bd0b0e213
Lars Huebner <lars.huebner@desy.de>
2021-02-08 18:40:50 +0100

    Code cleanup. Removed 'fast' mode, because it is much slower than pocketfft in my tests. Removed unused code in setup.py

--- czt.py
+++ czt.py
@@ -2,8 +2,8 @@
 
 Main reference:
 
-    Sukhoy, V., Stoytchev, A. Generalizing the inverse FFT off the unit 
-    circle. Sci Rep 9, 14443 (2019). 
+    Sukhoy, V., Stoytchev, A. Generalizing the inverse FFT off the unit
+    circle. Sci Rep 9, 14443 (2019).
     https://doi.org/10.1038/s41598-019-50234-9
 
 """
@@ -15,7 +15,8 @@ from scipy.signal import kaiser
 
 # CZT TRANSFORM --------------------------------------------------------------
 
-def czt(x, M=None, W=None, A=1.0, simple=False, t_method='ce', f_method='std'):
+
+def czt(x, M=None, W=None, A=1.0, simple=False, t_method="ce"):
     """Calculate Chirp Z-transform (CZT).
 
     Using an efficient algorithm. Solves in O(n log n) time.
@@ -28,24 +29,21 @@ def czt(x, M=None, W=None, A=1.0, simple=False, t_method='ce', f_method='std'):
         W (complex): complex ratio between points
         A (complex): complex starting point
         simple (bool): use simple algorithm?
-        t_method (str): Toeplitz matrix multiplication method. 'ce' for 
+        t_method (str): Toeplitz matrix multiplication method. 'ce' for
             circulant embedding, 'pd' for Pustylnikov's decomposition, 'mm'
             for simple matrix multiplication
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays. Warning:
-            'fast' doesn't seem to work very well. More testing required.
 
     Returns:
         np.ndarray: Chirp Z-transform
 
     """
-    
+
     N = len(x)
     if M is None:
         M = N
     if W is None:
         W = np.exp(-2j * np.pi / M)
-        
+
     if simple:
         k = np.arange(M)
         X = np.zeros(M, dtype=complex)
@@ -54,19 +52,16 @@ def czt(x, M=None, W=None, A=1.0, simple=False, t_method='ce', f_method='std'):
             X += x[n] * z ** -n
         return X
 
-    if f_method == 'fast':
-        print("Warning: 'fast' doesn't seem to work very well. More testing needed.")
-
     k = np.arange(N)
     X = W ** (k ** 2 / 2) * A ** -k * x
     r = W ** (-(k ** 2) / 2)
     k = np.arange(M)
     c = W ** (-(k ** 2) / 2)
-    if t_method.lower() == 'ce':
-        X = _toeplitz_mult_ce(r, c, X, f_method)
-    elif t_method.lower() == 'pd':
-        X = _toeplitz_mult_pd(r, c, X, f_method)
-    elif t_method.lower() == 'mm':
+    if t_method.lower() == "ce":
+        X = _toeplitz_mult_ce(r, c, x)
+    elif t_method.lower() == "pd":
+        X = _toeplitz_mult_pd(r, c, X)
+    elif t_method.lower() == "mm":
         X = np.matmul(toeplitz(r, c), X)
     else:
         print("t_method not recognized.")
@@ -77,7 +72,7 @@ def czt(x, M=None, W=None, A=1.0, simple=False, t_method='ce', f_method='std'):
     return X
 
 
-def iczt(X, N=None, W=None, A=1.0, t_method='ce', f_method='std'):
+def iczt(X, N=None, W=None, A=1.0, t_method="ce"):
     """Calculate inverse Chirp Z-transform (ICZT).
 
     Args:
@@ -85,12 +80,9 @@ def iczt(X, N=None, W=None, A=1.0, t_method='ce', f_method='std'):
         N (int): length of output array
         W (complex): complex ratio between points
         A (complex): complex starting point
-        t_method (str): Toeplitz matrix multiplication method. 'ce' for 
+        t_method (str): Toeplitz matrix multiplication method. 'ce' for
             circulant embedding, 'pd' for Pustylnikov's decomposition, 'mm'
             for simple matrix multiplication
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays. Warning:
-            'fast' doesn't seem to work very well. More testing required.
 
     Returns:
         np.ndarray: Inverse Chirp Z-transform
@@ -103,13 +95,14 @@ def iczt(X, N=None, W=None, A=1.0, t_method='ce', f_method='std'):
     if W is None:
         W = np.exp(-2j * np.pi / M)
 
-    return np.conj(czt(np.conj(X), M=N, W=W, A=A, t_method=t_method, f_method=f_method)) / M
+    return np.conj(czt(np.conj(X), M=N, W=W, A=A, t_method=t_method)) / M
 
 
 # OTHER TRANSFORMS -----------------------------------------------------------
 
+
 def dft(t, x, f=None):
-    """Convert signal from time-domain to frequency-domain using a Discrete 
+    """Convert signal from time-domain to frequency-domain using a Discrete
     Fourier Transform (DFT).
 
     Args:
@@ -124,7 +117,7 @@ def dft(t, x, f=None):
 
     if f is None:
         dt = t[1] - t[0]  # time step
-        Fs = 1 / dt       # sample frequency
+        Fs = 1 / dt  # sample frequency
         f = np.linspace(-Fs / 2, Fs / 2, len(t))
 
     X = np.zeros(len(f), dtype=complex)
@@ -135,7 +128,7 @@ def dft(t, x, f=None):
 
 
 def idft(f, X, t=None):
-    """Convert signal from time-domain to frequency-domain using an Inverse 
+    """Convert signal from time-domain to frequency-domain using an Inverse
     Discrete Fourier Transform (IDFT).
 
     Args:
@@ -165,6 +158,7 @@ def idft(f, X, t=None):
 
 # FREQ <--> TIME-DOMAIN CONVERSION -------------------------------------------
 
+
 def time2freq(t, x, f=None, f_orig=None):
     """Convert signal from time-domain to frequency-domain.
 
@@ -183,17 +177,17 @@ def time2freq(t, x, f=None, f_orig=None):
 
     # Input time array
     t1, t2 = t.min(), t.max()  # start / stop time
-    dt = t[1] - t[0]           # time step
-    Nt = len(t)                # number of time points
-    Fs = 1 / dt                # sampling frequency
+    dt = t[1] - t[0]  # time step
+    Nt = len(t)  # number of time points
+    Fs = 1 / dt  # sampling frequency
 
     # Output frequency array
     if f is None:
         f = np.linspace(-Fs / 2, Fs / 2, Nt)
     f1, f2 = f.min(), f.max()  # start / stop
-    df = f[1] - f[0]           # frequency step
-    bw = f2 - f1               # bandwidth
-    Nf = len(f)                # number of frequency points
+    df = f[1] - f[0]  # frequency step
+    bw = f2 - f1  # bandwidth
+    Nf = len(f)  # number of frequency points
 
     # Correction factor (normalization)
     if f_orig is not None:
@@ -228,23 +222,23 @@ def freq2time(f, X, t=None, t_orig=None):
 
     # Input frequency
     f1, f2 = f.min(), f.max()  # start / stop frequency
-    df = f[1] - f[0]           # frequency step
-    bw = f2 - f1               # bandwidth
-    fc = (f1 + f2) / 2         # center frequency
-    Nf = len(f)                # number of frequency points
-    t_alias = 1 / df           # alias-free interval
+    df = f[1] - f[0]  # frequency step
+    bw = f2 - f1  # bandwidth
+    fc = (f1 + f2) / 2  # center frequency
+    Nf = len(f)  # number of frequency points
+    t_alias = 1 / df  # alias-free interval
 
     # Output time
     if t is None:
         t = np.linspace(0, t_alias, Nf)
     t1, t2 = t.min(), t.max()  # start / stop time
-    dt = t[1] - t[0]           # time step
-    Nt = len(t)                # number of time points
-    Fs = 1 / dt                # sampling frequency
+    dt = t[1] - t[0]  # time step
+    Nt = len(t)  # number of time points
+    Fs = 1 / dt  # sampling frequency
 
     # Correction factor (normalization)
     if t_orig is not None:
-        k = (t.max() - t.min()) / df / (t_orig.max() - t_orig.min()) **2
+        k = (t.max() - t.min()) / df / (t_orig.max() - t_orig.min()) ** 2
     else:
         k = 1
 
@@ -267,13 +261,14 @@ def freq2time(f, X, t=None, t_orig=None):
 
 # WINDOW ---------------------------------------------------------------------
 
+
 def get_window(f, f_start=None, f_stop=None, beta=6):
     """Get Kaiser-Bessel window.
 
     See: https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.signal.kaiser.html
-    
+
     Args:
-        f (np.ndarray): frequency 
+        f (np.ndarray): frequency
         f_start (float): start frequency
         f_stop (float): stop frequency
         beta (float): KB parameter for Kaiser Bessel filter
@@ -291,22 +286,22 @@ def get_window(f, f_start=None, f_stop=None, beta=6):
 
     # Get corresponding indices
     idx_start = np.abs(f - f_start).argmin()
-    idx_stop  = np.abs(f - f_stop).argmin()
-    idx_span  = idx_stop - idx_start
+    idx_stop = np.abs(f - f_stop).argmin()
+    idx_span = idx_stop - idx_start
 
     # Make window
-    window = np.r_[np.zeros(idx_start),
-                    kaiser(idx_span, beta),
-                    np.zeros(len(f) - idx_stop)]
+    window = np.r_[
+        np.zeros(idx_start), kaiser(idx_span, beta), np.zeros(len(f) - idx_stop)
+    ]
 
-    return window 
+    return window
 
 
 def window(f, s, f_start=None, f_stop=None, beta=6, normalize=True):
     """Window frequency-domain data using Kaiser-Bessel filter.
-    
+
     Args:
-        f (np.ndarray): frequency 
+        f (np.ndarray): frequency
         s (np.ndarray): S-parameter
         f_start (float): start frequency
         f_stop (float): stop frequency
@@ -330,25 +325,24 @@ def window(f, s, f_start=None, f_stop=None, beta=6, normalize=True):
 
 # HELPER FUNCTIONS -----------------------------------------------------------
 
-def _toeplitz_mult_ce(r, c, x, f_method='std'):
+
+def _toeplitz_mult_ce(r, c, x):
     """Multiply Toeplitz matrix by vector using circulant embedding.
-    
+
     "Compute the product y = Tx of a Toeplitz matrix T and a vector x, where T
-    is specified by its first row r = (r[0], r[1], r[2],...,r[N-1]) and its 
+    is specified by its first row r = (r[0], r[1], r[2],...,r[N-1]) and its
     first column c = (c[0], c[1], c[2],...,c[M-1]), where r[0] = c[0]."
-    
+
     See algorithm S1 in Sukhoy & Stoytchev 2019 (full reference in README).
-    
+
     Args:
         r (np.ndarray): first row of Toeplitz matrix
         c (np.ndarray): first column of Toeplitz matrix
         x (np.ndarray): vector to multiply the Toeplitz matrix
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays.
-    
+
     Returns:
         np.ndarray: product of Toeplitz matrix and vector x
-        
+
     """
     N = len(r)
     M = len(c)
@@ -357,32 +351,30 @@ def _toeplitz_mult_ce(r, c, x, f_method='std'):
     n = int(2 ** np.ceil(np.log2(M + N - 1)))
     assert n >= M
     assert n >= N
-    chat = np.r_[c, np.zeros(n - (M + N - 1)), r[-(N-1):][::-1]]
+    chat = np.r_[c, np.zeros(n - (M + N - 1)), r[-(N - 1) :][::-1]]
     xhat = _zero_pad(x, n)
-    yhat = _circulant_multiply(chat, xhat, f_method)
+    yhat = _circulant_multiply(chat, xhat)
     y = yhat[:M]
     return y
 
 
-def _toeplitz_mult_pd(r, c, x, f_method='std'):
+def _toeplitz_mult_pd(r, c, x):
     """Multiply Toeplitz matrix by vector using Pustylnikov's decomposition.
-    
+
     Compute the product y = Tx of a Toeplitz matrix T and a vector x, where T
-    is specified by its first row r = (r[0], r[1], r[2],...,r[N-1]) and its 
+    is specified by its first row r = (r[0], r[1], r[2],...,r[N-1]) and its
     first column c = (c[0], c[1], c[2],...,c[M-1]), where r[0] = c[0].
-    
+
     See algorithm S3 in Sukhoy & Stoytchev 2019 (full reference in README).
-    
+
     Args:
         r (np.ndarray): first row of Toeplitz matrix
         c (np.ndarray): first column of Toeplitz matrix
         x (np.ndarray): vector to multiply the Toeplitz matrix
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays.
-    
+
     Returns:
         np.ndarray: product of Toeplitz matrix and vector x
-        
+
     """
     N = len(r)
     M = len(c)
@@ -401,24 +393,24 @@ def _toeplitz_mult_pd(r, c, x, f_method='std'):
     for k in range(1, n):
         c1[k] = 0.5 * (c[k] + r[n - k])
         c2[k] = 0.5 * (c[k] - r[n - k])
-    y1 = _circulant_multiply(c1, x, f_method)
-    y2 = _skew_circulant_multiply(c2, x, f_method)
+    y1 = _circulant_multiply(c1, x)
+    y2 = _skew_circulant_multiply(c2, x)
     y = y1[:M] + y2[:M]
     return y
 
 
 def _zero_pad(x, n):
     """Zero pad an array x to length n by appending zeros.
-    
+
     See algorithm S2 in Sukhoy & Stoytchev 2019 (full reference in README).
-    
+
     Args:
         x (np.ndarray): array x
         n (int): length of output array
-        
+
     Returns:
         np.ndarray: array x with padding
-        
+
     """
     m = len(x)
     assert m <= n
@@ -427,60 +419,44 @@ def _zero_pad(x, n):
     return xhat
 
 
-def _circulant_multiply(c, x, f_method='std'):
+def _circulant_multiply(c, x, f_method="std"):
     """Multiply a circulat matrix by a vector.
-    
-    Compute the product y = Gx of a circulat matrix G and a vector x, where G 
+
+    Compute the product y = Gx of a circulat matrix G and a vector x, where G
     is generated by its first column c=(c[0], c[1],...,c[n-1]).
 
     Runs in O(n log n) time.
 
     See algorithm S4 in Sukhoy & Stoytchev 2019 (full reference in README).
-    
+
     Args:
         c (np.ndarray): first column of circulant matrix G
         x (np.ndarray): vector x
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays.
-
     Returns:
         np.ndarray: product Gx
-    
+
     """
     n = len(c)
     assert len(x) == n
-    if f_method == 'std':
-        C = np.fft.fft(c)
-        X = np.fft.fft(x)
-        Y = np.empty(n, dtype=complex)
-        for k in range(n):
-            Y[k] = C[k] * X[k]
-        y = np.fft.ifft(Y)
-    elif f_method.lower() == 'fast':
-        C = _fft(c)
-        X = _fft(x)
-        Y = np.empty(n, dtype=complex)
-        for k in range(n):
-            Y[k] = C[k] * X[k]
-        y = _ifft(Y)
-    else:
-        print("f_method not recognized.")
-        raise ValueError
+    C = np.fft.fft(c)
+    X = np.fft.fft(x)
+    Y = np.empty(n, dtype=complex)
+    for k in range(n):
+        Y[k] = C[k] * X[k]
+    y = np.fft.ifft(Y)
     return y
 
 
-def _skew_circulant_multiply(c, x, f_method='std'):
+def _skew_circulant_multiply(c, x):
     """Multiply a skew-circulant matrix by a vector.
-    
+
     Runs in O(n log n) time.
-    
+
     See algorithm S7 in Sukhoy & Stoytchev 2019 (full reference in README).
-    
+
     Args:
         c (np.ndarray): first column of skew-circulant matrix G
         x (np.ndarray): vector x
-        f_method (str): FFT method. 'std' for standard FFT (from NumPy), or 
-            'fast' for a method that may be faster for large arrays.
 
     Returns:
         np.ndarray: product Gx
@@ -496,59 +472,7 @@ def _skew_circulant_multiply(c, x, f_method='std'):
     # k = np.arange(n, dtype=complex)
     # chat = c * np.exp(-1j * k * np.pi / n)
     # xhat = c * np.exp(-1j * k * np.pi / n)
-    y = _circulant_multiply(chat, xhat, f_method)
+    y = _circulant_multiply(chat, xhat)
     for k in range(n):
         y[k] = y[k] * np.exp(1j * k * np.pi / n)
     return y
-
-
-def _fft(x):
-    """FFT algorithm. Runs in O(n log n) time.
-
-    Args:
-        x (np.ndarray): input
-
-    Returns:
-        np.ndarray: FFT of x
-
-    """
-    n = len(x)
-    if n == 1:
-        return x
-    xe = x[0::2]
-    xo = x[1::2]
-    y1 = np.fft.fft(xe)
-    y2 = np.fft.fft(xo)
-    # Todo: simplify
-    y = np.zeros(n, dtype=complex)
-    for k in range(n // 2 - 1):
-        w = np.exp(-2j * np.pi * k / n)
-        y[k]            = y1[k] + w * y2[k]
-        y[k + (n // 2)] = y1[k] - w * y2[k]
-    return y
-
-
-def _ifft(y):
-    """IFFT algorithm. Runs in O(n log n) time.
-
-    Args:
-        y (np.ndarray): input
-
-    Returns:
-        np.ndarray: IFFT of y
-
-    """
-    n = len(y)
-    if n == 1:
-        return y
-    ye = y[0::2]
-    yo = y[1::2]
-    x1 = np.fft.ifft(ye)
-    x2 = np.fft.ifft(yo)
-    # TODO: simplify
-    x = np.zeros(n, dtype=complex)
-    for k in range(n // 2 -1):
-        w = np.exp(2j * np.pi * k / n)
-        x[k]            = (x1[k] + w * x2[k]) / 2
-        x[k + (n // 2)] = (x1[k] - w * x2[k]) / 2
-    return x
--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 """Install CZT package."""
 
 import io
-import os
 import sys
 from os import path
 
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
-
-import czt
 
 root = path.abspath(path.dirname(__file__))
 
+
 def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
+    encoding = kwargs.get("encoding", "utf-8")
+    sep = kwargs.get("sep", "\n")
     buf = []
     for filename in filenames:
         with io.open(path.join(root, filename), encoding=encoding) as f:
             buf.append(f.read())
     return sep.join(buf)
 
-long_description = read('README.md')
+
+long_description = read("README.md")
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -31,12 +31,13 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import pytest
+
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
+
 setup(
     name="czt",
-    # version=czt.__version__,
     version="0.0.2",
     author="John Garrett",
     author_email="garrettj403@gmail.com",
@@ -46,29 +47,36 @@ setup(
     keywords=[
         "z-transform",
         "signal-processing",
-        "dsp",],
-    # packages=find_packages(),
-    py_modules=['czt'],
+        "dsp",
+    ],
+    py_modules=["czt"],
     install_requires=[
-        'numpy',
-        'scipy',],
+        "numpy",
+        "scipy",
+    ],
     extras_require={
-        'testing': ['pytest'],
-        'examples': ['matplotlib', 'scikit-rf',],},
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
+        "testing": ["pytest"],
+        "examples": [
+            "matplotlib",
+            "scikit-rf",
+        ],
+    },
+    tests_require=["pytest"],
+    cmdclass={"test": PyTest},
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    platforms='any',
+    long_description_content_type="text/markdown",
+    platforms="any",
     classifiers=[
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",],
+        "Programming Language :: Python :: 3.7",
+    ],
     project_urls={
-        'Changelog': 'https://github.com/garrettj403/CZT/blob/master/CHANGES.md',
-        'Issue Tracker': 'https://github.com/garrettj403/CZT/issues',},
+        "Changelog": "https://github.com/garrettj403/CZT/blob/master/CHANGES.md",
+        "Issue Tracker": "https://github.com/garrettj403/CZT/issues",
+    },
     # scripts=[],
 )
